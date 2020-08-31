1712542db5a1111f065d96e38a4387002b4c7694
Mohit Soni <mohit19699soni@gmail.com>
2020-08-31 19:22:58 +0530

    Prod Commit

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 const socket = io('/')
 const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
-    host: 'peerjs-server.herokuapp.com',
+    host: '/',
     port: '9000'
 })
 const myVideo = document.createElement('video')
