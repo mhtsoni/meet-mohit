6d1a62becae7cf61352d92046ea807b5aee79ed7
Mohit Soni <mohit19699soni@gmail.com>
2020-09-01 12:37:21 +0530

    Prod Commit

--- public/script.js
+++ public/script.js
@@ -1,9 +1,9 @@
 const socket = io('/')
-const videoGrid = document.getElementById('video-grid')
 const myPeer = new Peer(undefined, {
 	host: "/",
 	port: "3001"
 });
+const videoGrid = document.getElementById('video-grid')
 const myVideo = document.createElement('video')
 myVideo.muted = true
 
