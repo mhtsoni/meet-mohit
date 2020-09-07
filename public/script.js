9522ec860baa6af3cb3511bc9fee8da7a6cfbf3d
Mohit Soni <mohit19699soni@gmail.com>
2020-09-07 15:40:22 +0530

    Comments Added

--- public/script.js
+++ public/script.js
@@ -1,37 +1,44 @@
-const socket = io('/')
-const myPeer = new Peer();
-const videoGrid = document.getElementById('video-grid')
-const myVideo = document.createElement('video')
-myVideo.muted = true
+const socket = io('/')//Passing path of static files to be served
+const myPeer = new Peer();//creating a new peer for each user
+const videoGrid = document.getElementById('video-grid') //Video Grid Object to render all videos
+const myVideo = document.createElement('video')//Video object to render users own video
+myVideo.muted = true// The user must not hear his own voice
 
-var audio_button = document.getElementById("audio_button");
-var video_button = document.getElementById("video_button");
-var invite= document.getElementById("invite");
+var audio_button = document.getElementById("audio_button");//Audio ON-OFF button
+var video_button = document.getElementById("video_button");//Video ON-OFF button
+var invite= document.getElementById("invite");//Invite Link Button
+
+//Function to copy invite link onClick
 invite.onclick = function(){
-
-        var dummy = document.createElement('input'),
-          text = window.location.href;
-        document.body.appendChild(dummy);
-        dummy.value = text;
-        dummy.select();
-        document.execCommand('copy');
-        document.body.removeChild(dummy);
-        alert('Invite Link copied to your clipboard send this link to atendies');
+        var dummy = document.createElement('input');//Create Input Element
+        text = window.location.href;//Set text variable
+        document.body.appendChild(dummy);//Adding dummy Input to body
+        dummy.value = text;//Setting the dummy input value
+        dummy.select();//Select the dummy input
+        document.execCommand('copy');//Execute copy command
+        document.body.removeChild(dummy);//Remove dummy input
+        alert('Invite Link copied to your clipboard send this link to atendies');//Give alert to user
 }
-let mic_switch = true;
-let video_switch = true;
+let mic_switch = true;//Initially mic is on
+let video_switch = true;// Initially Video of user is on
 
-const peers = {}
+const peers = {} //Peers object to store which users have already joined
+
+//get audio and video of user
 navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true
 }).then((stream) =>{
     addVideoStream(myVideo, stream)
     // Audio Video Toggler
-    var localStream=stream //Get stream
+    var localStream=stream //Get stream into a variable
+
+    //Video Controls
     video_button.onclick = function() {
-        if(localStream != null && localStream.getVideoTracks().length > 0){
-            video_switch = !video_switch;
+        if(localStream != null && localStream.getVideoTracks().length > 0){//If video exists
+            video_switch = !video_switch;//Toggle video of user
+            
+            //Toggle Video Button Icon
             if(video_switch){
                 document.getElementById("vid").src='./HomePage/img/video_on.png';
             }
@@ -41,10 +48,13 @@ navigator.mediaDevices.getUserMedia({
             localStream.getVideoTracks()[0].enabled = video_switch;
         }
     }
+    //Audio Controls
     audio_button.onclick = function() {
+
         if(localStream != null && localStream.getAudioTracks().length > 0){
-            mic_switch = !mic_switch;
+            mic_switch = !mic_switch;//Toggle Mic
             
+            //Toggle Audio Button Icon
             if(mic_switch){
                 document.getElementById("aud").src='./HomePage/img/contact-icon.png';
             }
@@ -55,7 +65,7 @@ navigator.mediaDevices.getUserMedia({
         }
     }
 
-
+    //On execution of call event
     myPeer.on('call',(call)=>{
         call.answer(stream)
         const video = document.createElement('video')
--- server.js
+++ server.js
@@ -1,23 +1,23 @@
-const express= require('express')
-const app = express()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
-const {v4 : uuidv4} = require('uuid')
+const express= require('express')// Express Package
+const app = express()// Calling the express module
+const server = require('http').Server(app)//Creating an http server
+const io = require('socket.io')(server)//Socket.io Instance
+const {v4 : uuidv4} = require('uuid')//Lib. to create random urls with uuidv4 function
 
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.set('view engine', 'ejs')//Setting the view Engine
+app.use(express.static('public'))//creating a relative path to look for static files
 app.get('/', (req,res)=>{
-    res.render('index')
+    res.render('index')//Load the Index page as Home Page
 })
 app.get('/start', (req,res)=>{
-    res.redirect(`/${uuidv4()}`)
+    res.redirect(`/${uuidv4()}`)//Redirect to a new random room
 })
 app.get('/:room',(req,res)=>{
-    res.render('room', {roomId : req.params.room})
+    res.render('room', {roomId : req.params.room})//passing the romId fetched from url and rendering room Page
 })
-io.on('connection', (socket) =>{
-    socket.on('join-room',(roomId,userId)=>{
-        socket.join(roomId)
+io.on('connection', (socket) =>{//When connection event occurs
+    socket.on('join-room',(roomId,userId)=>{//Custom event that gets roomId and userId from client side
+        socket.join(roomId)//Join using provided Id
         socket.to(roomId).broadcast.emit('user-connected', userId)
         socket.on('disconnect',()=>{
             socket.to(roomId).broadcast.emit('user-disconnected',userId)
